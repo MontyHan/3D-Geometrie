6592aebe6085bd70975470409aae3a87d493f24c
MontyHan <b.hanssen@fsg-geldern.de>
2026-06-03 23:21:55 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import { initXR } from './core/xr.js';
 import { initControllers, updateControllers } from './core/controllers.js';
 import { initTeleport, updateTeleport } from './core/teleport.js';
 import { initGrid } from './core/grid.js';
+import { initInteraction, updateInteraction } from './core/interaction.js';
+import { initPoints } from './core/points.js';
+
 
 let scene, camera, renderer;
 let rig; // ✅ NEU
@@ -31,6 +34,9 @@ function init() {
 
     initXR(renderer);
     initGrid(scene);
+    initPoints(scene);
+    initInteraction(renderer, rig, scene);
+
 
     // Licht
     const light = new THREE.HemisphereLight(0xffffff, 0x444444, 1.2);
@@ -62,6 +68,8 @@ function animate() {
 
         updateControllers();
         updateTeleport();
+        updateInteraction();
+
 
         renderer.render(scene, camera);
     });
